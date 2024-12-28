6836dba9894e1a11220466a7c2cc4b1fd8362002
Walmond3 <130675454+Walmond3@users.noreply.github.com>
2024-12-29 01:19:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.title('My Data Product')
+
+page = st.sidebar.selectboc('Select a page', ['Overview', 'Chart', 'Prediction', 'Portfolio'])
+
+if page == 'Overview':
+  import pages.overview
+elif page == 'Chart':
+  import pages.chart
+elif page == 'Prediction':
+  import pages.prediction
+elif page == 'Portfolio':
+  import pages.portfolio
+                                            
