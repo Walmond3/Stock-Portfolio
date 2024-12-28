6e9557b3b1a6bd237d858366a6001a39cc63f68f
Walmond3 <130675454+Walmond3@users.noreply.github.com>
2024-12-29 01:49:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,4 @@ import streamlit as st
 
 st.title('My Data Product')
 
-st.sidebar.title('Navigation')
-
-if st.sidebar.button('Overview'):
-  import pages.overview
-  pages.overview.app()
-elif st.sidebar.button('Chart'):
-  import pages.chart
-elif st.sidebar.button('Prediction'):
-  import pages.prediction
-elif st.sidebar.button('Portfolio'):
-  import pages.portfolio
                                             
