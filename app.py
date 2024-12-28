94db2485a2f774dea2c1667ff552f091f7c8dd01
Walmond3 <130675454+Walmond3@users.noreply.github.com>
2024-12-29 01:41:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 st.title('My Data Product')
 
-st.sidebar.title('Page')
+st.sidebar.title('Navigation')
 
 if st.sidebar.button('Overview'):
   import pages.overview
+  pages.overview.app()
 elif st.sidebar.button('Chart'):
   import pages.chart
 elif st.sidebar.button('Prediction'):
